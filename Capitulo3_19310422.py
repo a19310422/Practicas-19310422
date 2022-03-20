3bfe9d7aa57fbefa87de322cd678f08dd64c0337
a19310422 <101608957+a19310422@users.noreply.github.com>
2022-03-20 10:52:24 -0600

    Add files via upload

---
+++ Capitulo3_19310422.py
@@ -0,0 +1,5 @@
+#CAPITULO 3,  STRINGS 1 
+mensaje1 = "Hola mundo" #Le asignamos a la variable mensaje1 un volor tipo string
+mensaje2 = 'Adios mundo' #Le asignamos a la variable mensaje1 un volor tipo string
+print ("\n" , mensaje1, ', doble "comilla"') #Se imprime el mensaje con doble comilla
+
