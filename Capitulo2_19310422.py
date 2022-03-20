a2f9a48034eb5dae1736e80be050894c10e2eb69
a19310422 <101608957+a19310422@users.noreply.github.com>
2022-03-20 10:51:54 -0600

    Add files via upload

---
+++ Capitulo2_19310422.py
@@ -0,0 +1,9 @@
+
+#Capitulo 2
+mensaje="Practica 1" #Se le asigna el mensaje a la vaiable
+num1=2 #Se le asigna el valor a la variable
+num2=48 #Se le asigna el valor a la variable
+suma=num1+num2 #Se realiza la operacion en la variable suma
+print(mensaje) #Se imprime la vraiable mensaje
+print("\n Resultado de la suma:", suma) #Se imprime la vraiable suma
+
