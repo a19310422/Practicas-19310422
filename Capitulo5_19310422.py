a72a65c7c3e3e0949db772bf43f6c5b6242b27e7
a19310422 <101608957+a19310422@users.noreply.github.com>
2022-03-20 11:00:41 -0600

    Add files via upload

---
+++ Capitulo5_19310422.py
@@ -0,0 +1,16 @@
+
+#Capitulo 5
+
+#Ejercicio 12
+name = "Juan Manuel Andrade Iñiguez".title() 
+print("\n \t", name) 
+
+#Ejercicio 13
+name2 = "Juan Manuel Andrade Iñiguez".lower() 
+print("\n \t", name2) #Imprimimos name2 con todas las letras minusculas
+
+#Ejercicio 14
+name3 = "Juan Manuel Andrade Iñiguez".upper() 
+print("\n \t", name3) #Imprimimos name2 con todas las letras mayusculas
+
+
