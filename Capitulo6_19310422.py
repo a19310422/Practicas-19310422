1650bc864c2459b1e1eaec05286af49441028fab
a19310422 <101608957+a19310422@users.noreply.github.com>
2022-03-20 11:14:09 -0600

    Add files via upload

---
+++ Capitulo6_19310422.py
@@ -0,0 +1,3 @@
+#Capitulo 6
+print("Lenguajes de programacion:")
+print(" \t\n-Python.\n-JavaScript.\n-Java.\n-PHP.\n-TypeScript.\n-SQL.\n-COBOL.") #Imprimimos en forma de lista, usando saltos de linea
