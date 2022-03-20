1e07025a803a39182936b0da1e5905ebaf9024c0
a19310422 <101608957+a19310422@users.noreply.github.com>
2022-03-20 11:15:18 -0600

    Add files via upload

---
+++ Capitulo4_19310422.py
@@ -0,0 +1,27 @@
+#Capitulo 4
+
+#Ejercicio 7 
+mensaje = "Hola" + " como estas?" #Le asignamos a la vafiable mensaje un valor tipo string pero concatenado
+print("\n \t", mensaje) #Imprimimos mensaje
+
+#Ejercicio 8
+mens1= "Ejercicio"  #Le asignamos a la variable mens1 un valor tipo string pero concatenado
+mens2= " Ocho" #Le asignamos a la variable mens2 un valor tipo string pero concatenado
+conc= mens1 + mens2 #se concatena mens1 y mns2 a traves de una tercer variable
+print("\n \n \t", conc) #Imprimimos la concatenacion
+
+#Ejercicio 9 
+mnj= "Hasta" 
+print( "\n\t", mnj + " luego" ) 
+
+#Ejercicio 10
+nom1 = "Juan" 
+nom2 = "Manuel"
+lastn = "Andrade" 
+lastn2 = "Iñiguez"
+name = nom1 + " " + nom2 + " " + lastn + " " + lastn2  
+print( "\n\t", name) 
+
+#Ejercicio 11
+num = "9" + "19" 
+print("\n\t", num) 
