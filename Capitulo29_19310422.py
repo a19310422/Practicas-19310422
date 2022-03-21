8d751c912978487cb77b8018c4d80287af871c96
a19310422 <101608957+a19310422@users.noreply.github.com>
2022-03-20 22:01:29 -0600

    Add files via upload

---
+++ Capitulo29_19310422.py
@@ -0,0 +1,4 @@
+#Capitulo 29                                  
+colores = ('rojo','azul','verde','amarillo')#definimos la lista
+for x in colores:#Se abre un for y  usamos la variable x, que tomará cada posicion de la lista, para luego imprimirla
+    print('El color es: ' + x + '.')
